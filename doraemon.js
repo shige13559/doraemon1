b37e1b149a22dda40b094c2d0820ad5a3a5cd262
原田茂大 <sapori946@gmail.com>
2019-09-09 14:47:38 +0800

    未完成

---
+++ doraemon.js
@@ -0,0 +1,8 @@
+// function button3(){
+    // nose = document.getElementById("base");
+    // nose.backgroundcolor = "yellow"; 
+// }
+
+function button3( newColor ) {
+    document.getElementById('base').style.backgroundColor = newColor;
+}
