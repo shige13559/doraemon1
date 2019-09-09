45a1e940fd02b27851b1e0bfc2bb62fec724563b
原田茂大 <sapori946@gmail.com>
2019-09-09 22:11:28 +0800

    もう少し

--- doraemon.js
+++ doraemon.js
@@ -3,6 +3,25 @@
     // nose.backgroundcolor = "yellow"; 
 // }
 
-function button3( newColor ) {
-    document.getElementById('base').style.backgroundColor = newColor;
+// function button3( newColor ) {
+    // document.getElementById('button').style.backgroundColor = newColor;
+// }
+
+// document.getElementById("base").onclick = function() {
+    // document.getElementById("button").style.backgroundcolor = "yellow";
+// };
+
+// function changeColor(button){
+    // var obj = document.getElementById(button);
+    // obj.style.backgroundColor = '#ff0000';  //背景色を赤にする
+// }
+
+
+
+function color2(){
+    document.bgColor = "#fbeae6";
+}
+function color3(base){
+    let hana=document.getElementById(base);
+    hana.style.backgroundColor = "#ffff00"; 
 }
